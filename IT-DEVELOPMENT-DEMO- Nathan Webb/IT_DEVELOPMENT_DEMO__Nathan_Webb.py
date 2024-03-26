1de3a03636dae3de43d27c66de0936ae7b386abb
Nathan Webb <webbnathana@gmail.com>
2024-03-26 12:31:02 -0700

    added dates, and day functionality to visual calendar

--- IT-DEVELOPMENT-DEMO- Nathan Webb/IT_DEVELOPMENT_DEMO__Nathan_Webb.py
+++ IT-DEVELOPMENT-DEMO- Nathan Webb/IT_DEVELOPMENT_DEMO__Nathan_Webb.py
@@ -1,15 +1,16 @@
 import pygame, sys,os
 from datetime import date, datetime
 
-def calenderCreate(dateObj):
-    dateList = []
 
-    firstDayofWeek = date(dateObj.year, dateObj.month, 1).weekday()
-    
+def daysInCurrentMonth(dateObj):
     if(dateObj.month != 12):
         daysInMonth = date(dateObj.year, dateObj.month + 1, 1) - date(dateObj.year, dateObj.month, 1)
     else:
         daysInMonth = date(dateObj.year + 1, 1, 1) - date(dateObj.year, dateObj.month, 1)
+    return daysInMonth    
+
+def calenderCreate(dateObj, daysInMonth):
+    dateList = []
 
     for i in range(1, daysInMonth.days + 1):
         dateList.append(date(dateObj.year, dateObj.month, i))
@@ -29,8 +30,12 @@ def moveMonthRight(dateObj):
         dateObj = date(dateObj.year, dateObj.month + 1, dateObj.day)
     return dateObj
 
-def textDisplay():
-    font = pygame.font.SysFont("Times New Roman")
+def textDisplay(text, x, y):
+    font = pygame.font.SysFont("Times New Roman", 30)
+    text = font.render(text, False, (0, 0, 0))
+    center = gameDisplay.get_rect().center
+    gameDisplay.blit(text, (center[0] + x, center[1] + y))
+    text.get_rect().center
     
 
 def calenderDisplay(calenderList, gameDisplay):
@@ -38,18 +43,30 @@ def calenderDisplay(calenderList, gameDisplay):
     
     for i in range(len(calenderList)):
         textDisplay()
+        
+def dayDisplay(dateObj):
+    firstDayofWeek = date(dateObj.year, dateObj.month, 1).weekday()
+    row = 0
+    iteration = firstDayofWeek
+    
+    for i in range (daysInCurrentMonth(dateObj).days):
+        textDisplay(f'{i + 1}', -355 + (97 * iteration), -245 + row * 96)
+        iteration += 1
+        if((iteration * 99) - 355 > 330):
+            row += 1
+            iteration = 0
 
 #standard pygame initialization
 pygame.init()
 gameDisplay = pygame.display.set_mode((1280,720))
 pygame.display.set_caption('Calender Application')
-calendarImg = pygame.image.load(os.path.join(".","images","CalendarDrawing.jpg"))
+calendarImg = pygame.image.load(os.path.join(".","images","CalendarDrawing.png"))
 arrowImg = pygame.image.load(os.path.join(".","images","arrow.png"))
 arrowImgLeft = pygame.transform.flip(arrowImg, 1, 0)
 
 calenderList = []
 dateObj = datetime.now()
-calenderList = calenderCreate(dateObj)
+calenderList = calenderCreate(dateObj, daysInCurrentMonth(dateObj))
 print(calenderList)
 
 while True:
@@ -58,6 +75,8 @@ while True:
     gameDisplay.blit(calendarImg, calendarImg.get_rect(center = gameDisplay.get_rect().center)) #Obtained from StackOverflow
     arrowBlit = gameDisplay.blit(arrowImg, [650,-285])
     leftArrowBlit = gameDisplay.blit(arrowImgLeft, [-340,-285])
+    textDisplay(f'{dateObj.month} / {dateObj.year}', -50, -350)
+    dayDisplay(dateObj)
     pygame.display.flip()
     
     for event in pygame.event.get():
@@ -66,22 +85,22 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN: #This will be user clicking arrows and or grids
             if leftArrowBlit.collidepoint(pygame.mouse.get_pos()):
                 dateObj = moveMonthLeft(dateObj)
-                calenderList = calenderCreate(dateObj)
+                calenderList = calenderCreate(dateObj, daysInCurrentMonth(dateObj))
                 print(calenderList)
             if arrowBlit.collidepoint(pygame.mouse.get_pos()):
                 dateObj = moveMonthRight(dateObj)
-                calenderList = calenderCreate(dateObj)
+                calenderList = calenderCreate(dateObj, daysInCurrentMonth(dateObj))
                 print(calenderList)
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 sys.exit()
             if event.key == pygame.K_LEFT:
                 dateObj = moveMonthLeft(dateObj)
-                calenderList = calenderCreate(dateObj)
+                calenderList = calenderCreate(dateObj, daysInCurrentMonth(dateObj))
                 print(calenderList)
             if event.key == pygame.K_RIGHT:
                 dateObj = moveMonthRight(dateObj)
-                calenderList = calenderCreate(dateObj)
+                calenderList = calenderCreate(dateObj, daysInCurrentMonth(dateObj))
                 print(calenderList)
     
     
