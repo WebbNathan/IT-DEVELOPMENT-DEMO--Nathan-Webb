5c4b01db022e06b7401e4eca131d30bbe8b9f091
Nathan Webb <webbnathana@gmail.com>
2024-03-27 11:04:26 -0700

    practically final product, all goals for project function

--- IT-DEVELOPMENT-DEMO- Nathan Webb/IT_DEVELOPMENT_DEMO__Nathan_Webb.py
+++ IT-DEVELOPMENT-DEMO- Nathan Webb/IT_DEVELOPMENT_DEMO__Nathan_Webb.py
@@ -1,4 +1,17 @@
-import pygame, sys,os
+'''
+Written by Nathan Webb; netid: webbn; Code demo for IT Development job position; March 27th 2024
+
+Code runs a calender application using pygame. The calender allows for the user to navigate to years and months,
+and displays the corresponding dates and weekdays. The user can click on a text box at the bottom of the page that
+allows for numerical input, which when entered, brings the user to another page that allows for notes to be written,
+as one would on a physical calender. What the user types is saved to a json file with dates as keys, and is saved 
+to the users computer.
+
+github repository: https://github.com/WebbNathan/IT-DEVELOPMENT-DEMO--Nathan-Webb
+
+'''
+
+import pygame, sys,os, json
 from datetime import date, datetime
 
 def daysInCurrentMonth(dateObj):
@@ -35,31 +48,65 @@ def textDisplay(text, x, y):
     center = gameDisplay.get_rect().center
     gameDisplay.blit(text, (center[0] + x, center[1] + y))
     text.get_rect().center
-
-def calendarDisplay(calendarList, gameDisplay):
-    firstDayofWeek = date(dateObj.year, dateObj.month, 1).weekday()
-    
-    for i in range(len(calendarList)):
-        textDisplay()
-        
+             
 def dayDisplay(dateObj):
     firstDayofWeek = date(dateObj.year, dateObj.month, 1).weekday()
     row = 0
     iteration = firstDayofWeek
     
     for i in range (daysInCurrentMonth(dateObj).days):
-        textDisplay(f'{i + 1}', -355 + (97 * iteration), -245 + row * 96)
+        textDisplay(f'{i + 1}', -300 + (97 * iteration), -245 + row * 96)
         iteration += 1
         if((iteration * 99) - 355 > 330):
             row += 1
             iteration = 0
             
-def calendarTaskView(textBoxText):
-    while True:
-        print(textBoxText)
-        break
+def jsonInit():
+    jsonfile = open('dates.json')
+    jsondict = json.load(jsonfile)
+    jsonfile.close()
+    return jsondict
             
-    
+def calendarTaskView(gameDisplay, dateString):
+    broke = False
+    string = ""
+    jsondict = jsonInit()
+    if dateString in jsondict:
+       string = jsondict[dateString]
+                
+    while not broke:
+      
+        gameDisplay.fill("white")
+        calendarView = pygame.image.load(os.path.join(".","images","calendarviewimg2.png"))
+        greenArrow = pygame.image.load(os.path.join(".","images","greenarrowcrop.png"))
+        gameDisplay.blit(calendarView, [330,40])
+        greenArrowBlit = gameDisplay.blit(greenArrow, [350, 675])
+        textDisplay(string, -200,-100)
+        textDisplay(dateString[7:9], -10,250)
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if greenArrowBlit.collidepoint(pygame.mouse.get_pos()):
+                    broke = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_BACKSPACE:
+                    if string != "":
+                        string = string.rstrip(string[-1])
+                else:
+                    string += event.unicode
+        
+    if string != "" or dateString in jsondict:
+        if dateString not in jsondict:
+            jsondict[dateString] = string
+        elif string == "":
+            del jsondict[dateString]
+        else:
+            jsondict.update({dateString:string})
+        with open ('dates.json', 'w') as file:
+            file.write(json.dumps(jsondict))
+        
 
 def gameLoop(gameDisplay, calendarList, calendarImg, arrowImg, arrowImgLeft, textBox, dateObj):
     textBoxActive = False
@@ -67,21 +114,22 @@ def gameLoop(gameDisplay, calendarList, calendarImg, arrowImg, arrowImgLeft, tex
     while True:
         gameDisplay.fill("white")
         gameDisplay.blit(calendarImg, calendarImg.get_rect(center = gameDisplay.get_rect().center)) #Obtained from StackOverflow
-        arrowBlit = gameDisplay.blit(arrowImg, [650,-285])
-        leftArrowBlit = gameDisplay.blit(arrowImgLeft, [-340,-285])
-        textBoxBlit = gameDisplay.blit(textBox, [460,600])
+        arrowBlit = gameDisplay.blit(arrowImg, [1000,30])
+        leftArrowBlit = gameDisplay.blit(arrowImgLeft, [90,35])
+        textBoxBlit = gameDisplay.blit(textBox, [505,590])
         textDisplay(f'{dateObj.month} / {dateObj.year}', -50, -350)
+        textDisplay(textBoxText, -100, 240)
         dayDisplay(dateObj)
         pygame.display.flip()
         
         if (textBoxText != "") and (not textBoxActive):
-            calendarTaskView(textBoxText)
+            calendarTaskView(gameDisplay, f"{dateObj.year}-{dateObj.month}-{textBoxText}")
             textBoxText = ""
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT: 
                 sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN: #This will be user clicking arrows and or grids
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 if leftArrowBlit.collidepoint(pygame.mouse.get_pos()):
                     dateObj = moveMonthLeft(dateObj)
                     calendarList = calendarCreate(dateObj, daysInCurrentMonth(dateObj))
@@ -94,6 +142,9 @@ def gameLoop(gameDisplay, calendarList, calendarImg, arrowImg, arrowImgLeft, tex
                 if textBoxActive:
                     if event.key == pygame.K_RETURN or event.key == pygame.K_ESCAPE:
                         textBoxActive = False
+                    elif event.key == pygame.K_BACKSPACE:
+                        if textBoxText != "":
+                            textBoxText = textBoxText.rstrip(textBoxText[-1])
                     else:
                         textBoxText += event.unicode
                 if event.key == pygame.K_ESCAPE:
@@ -110,8 +161,8 @@ def gameLoop(gameDisplay, calendarList, calendarImg, arrowImg, arrowImgLeft, tex
 pygame.init()
 gameDisplay = pygame.display.set_mode((1280,720))
 pygame.display.set_caption('calendar Application')
-calendarImg = pygame.image.load(os.path.join(".","images","CalendarDrawing.png"))
-arrowImg = pygame.image.load(os.path.join(".","images","arrow.png"))
+calendarImg = pygame.image.load(os.path.join(".","images","calendardrawingnew.png"))
+arrowImg = pygame.image.load(os.path.join(".","images","arrownew.png"))
 arrowImgLeft = pygame.transform.flip(arrowImg, 1, 0)
 textBox = pygame.image.load(os.path.join(".","images","textboxcropped.png"))
 textBox = pygame.transform.scale_by(textBox, 0.9)
